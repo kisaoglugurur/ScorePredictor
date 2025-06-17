a6191c703462c0c901277bf7f91a52a354c8e8cb
Gurur Kısaoğlu <kisaoglugurur@gmail.com>
2025-06-17 08:36:47 +0300

    Initial Commit

---
+++ ScorePredictor/ScorePredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorePredictorApp.swift
+//  ScorePredictor
+//
+//  Created by Gurur on 17.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScorePredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
